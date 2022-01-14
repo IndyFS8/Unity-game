542edae91fd8e6fbb6d839faaa904c3c38c319bb
08dbirkeland <74902288+08dbirkeland@users.noreply.github.com>
2022-01-14 15:06:09 +0000

    indy is dumb

--- Assets/Scripts/CameraInversion.cs
+++ Assets/Scripts/CameraInversion.cs
@@ -8,7 +8,7 @@ public class CameraInversion : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false
+        Cursor.visible = false;
         CinemachineCore.GetInputAxis = getAxisCustom;
     }
 

9cf2cddbc387836131b2de2012ddaeb9c91a165f
08dbirkeland <74902288+08dbirkeland@users.noreply.github.com>
2022-01-17 09:39:01 +0000

    Indy is a failure

--- Assets/Scripts/ProjectileSpawn.cs
+++ Assets/Scripts/ProjectileSpawn.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ProjectileSpawn : MonoBehaviour
 {
     public GameObject magic;
-
+    public GameObject kangaroo;
     public float speed;
     // Start is called before the first frame update
     void Start()
@@ -16,14 +16,17 @@ public class ProjectileSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 newRotation = new Vector3(kangaroo.transform.eulerAngles.x, kangaroo.transform.eulerAngles.y,
+            kangaroo.transform.eulerAngles.z);
         if (Input.GetKeyDown("z"))
         {
 
             GameObject magicProjectile = Instantiate(magic, transform.position + (transform.right * 2),
                 Quaternion.identity);
+            magicProjectile.transform.eulerAngles = newRotation;
             Rigidbody magicRB = magicProjectile.GetComponent<Rigidbody>();
-            magicRB.AddRelativeForce(Vector3.forward*speed);
-            Destroy(magicRB,3f);
+            magicRB.AddForce(magicRB.transform.forward * speed );
+            Destroy(magicProjectile,3f);
         }
     }
 }

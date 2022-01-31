2e21217b562ae0e04b7a21500a5b3f9fe972b75c
08dbirkeland <74902288+08dbirkeland@users.noreply.github.com>
2022-01-31 09:35:32 +0000

    you

--- Assets/Scripts/ProjectileSpawn.cs
+++ Assets/Scripts/ProjectileSpawn.cs
@@ -7,13 +7,15 @@ public class ProjectileSpawn : MonoBehaviour
     public GameObject magic;
     public GameObject magic2;
     public GameObject kangaroo;
+
     public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hi");
         Cursor.visible = false;
-        Physics.IgnoreCollision(magic2.GetComponent<Collider>(),kangaroo.GetComponent<Collider>());
+        Physics.IgnoreCollision(magic2.GetComponent<Collider>(), kangaroo.GetComponent<Collider>());
     }
 
     // Update is called once per frame
@@ -28,32 +30,35 @@ public class ProjectileSpawn : MonoBehaviour
                 Quaternion.identity);
             magicProjectile.transform.eulerAngles = newRotation;
             Rigidbody magicRB = magicProjectile.GetComponent<Rigidbody>();
-            magicRB.AddForce(magicRB.transform.forward * speed );
-            Destroy(magicProjectile,3f);
+            magicRB.AddForce(magicRB.transform.forward * speed);
+            Destroy(magicProjectile, 3f);
         }
 
         if (Input.GetKeyDown("e"))
         {
             for (int i = 0; i < 6; i++)
-            { 
-                
-                if (kangaroo.transform.eulerAngles.y + i * 60 <= 360)
+            {
+                if (i != 3)
                 {
-                    currentRotation.y = kangaroo.transform.eulerAngles.y + i * 60;
+
+                    if (kangaroo.transform.eulerAngles.y + i * 60 <= 360)
+                    {
+                        currentRotation.y = kangaroo.transform.eulerAngles.y + i * 60;
+                    }
+                    else
+                    {
+                        currentRotation.y = kangaroo.transform.eulerAngles.y + i * 60 - 360;
+                    }
+
+                    GameObject magicProjectile = Instantiate(magic2, transform.position,
+                        Quaternion.identity);
+                    currentRotation.x = 0;
+                    currentRotation.z = 0;
+                    magicProjectile.transform.eulerAngles = currentRotation;
+                    Rigidbody magicRB = magicProjectile.GetComponent<Rigidbody>();
+                    magicRB.AddForce(magicRB.transform.forward * speed);
+                    Destroy(magicProjectile, 3f);
                 }
-                else
-                {
-                    currentRotation.y = kangaroo.transform.eulerAngles.y + i * 60 - 360;
-                }
-                
-                GameObject magicProjectile = Instantiate(magic2, transform.position + (transform.up * 2f),
-                    Quaternion.identity);
-                currentRotation.x = 0;
-                currentRotation.z = 0;
-                magicProjectile.transform.eulerAngles = currentRotation;
-                Rigidbody magicRB = magicProjectile.GetComponent<Rigidbody>();
-                magicRB.AddForce(magicRB.transform.forward * speed );
-                Destroy(magicProjectile,3f);
             }
         }
     }

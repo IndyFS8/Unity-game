b0f7f5aea9e643a46e09b2b31c3e727dd26f9c3d
08dbirkeland <74902288+08dbirkeland@users.noreply.github.com>
2022-02-04 14:11:38 +0000

    Gorilla

--- Assets/Scripts/Enemy/DeadGorilla.cs
+++ Assets/Scripts/Enemy/DeadGorilla.cs
@@ -1,10 +1,14 @@
+using System;
+using Random= System.Random;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DeadGorilla : MonoBehaviour
 {
+
     public GameObject Gorilla;
     public float health;
     public GameObject Kangaroo;
@@ -18,6 +22,9 @@ public class DeadGorilla : MonoBehaviour
 
     private void OnTriggerEnter(Collider col)
     {
+        Random rnd = new Random();
+        var x = rnd.Next(-130, 100);
+        var z = rnd.Next(-200, 200);
         if (col.gameObject.CompareTag("Weapon"))
         {
             health -= 1;
@@ -26,7 +33,7 @@ public class DeadGorilla : MonoBehaviour
             
             if (health < 1)
             {
-                Death();
+                Death(x,z);
             }
 
         }  
@@ -39,14 +46,25 @@ public class DeadGorilla : MonoBehaviour
         
     }
 
-    public void Death()
+    public void Death(int x, int z)
     {
+        var position = new Vector3(x, 80, z);
+        var raycastResult = Physics.Raycast(position, Vector3.down, out var hit);
+        if (!raycastResult)
+        {
+            throw new Exception($"Could not find a spawn for {this}");
+        }
+
         Destroy(Gorilla);
-        
+        var newGowilla = Instantiate(Gorilla, hit.point + 2 *Vector3.up, Quaternion.identity);
+        newGowilla.GetComponent<NavMeshAgent>().enabled = true;
+        newGowilla.GetComponent<EnemyController>().enabled = true;
+
     }
 
     public void KangyDeath()
     {
         Destroy(Kangaroo);
+        
     }
 }
